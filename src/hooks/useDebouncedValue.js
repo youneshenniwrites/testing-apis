92e143f27a998509c6140354879d4ded3cd1c28e
Younes Henni <youneshenniwrites@gmail.com>
2025-03-17 17:22:22 +0000

    Update useDebouncedValue.js

--- src/hooks/useDebouncedValue.js
+++ src/hooks/useDebouncedValue.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useDeboucedValue = (value, delay = 300) => {
-  const [debouncedValue, setDebouncedSearch] = useState("");
+  const [debouncedValue, setDebouncedSearch] = useState(value);
 
   useEffect(() => {
     const handler = setTimeout(() => {
